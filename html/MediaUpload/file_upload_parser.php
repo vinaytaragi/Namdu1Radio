541cc3ec9b6bd17868d325b616b64379dbde8720
karan100010 <karanjoshi1993@gmail.com>
2021-05-30 06:15:19 +0530

    added size control to online server

--- html/MediaUpload/file_upload_parser.php
+++ html/MediaUpload/file_upload_parser.php
@@ -9,9 +9,13 @@ if (!$fileTmpLoc) { // if file not chosen
     echo "ERROR: Please browse for a file before clicking the upload button.";
     exit();
 }
+if($fileSize<	1,000,000 ){
 if(move_uploaded_file($fileTmpLoc, "/home/pi/Namdu1Radio/html/.upload/gencat/$fileName")){
     echo "$fileName upload is complete";
 } else {
     echo "move_uploaded_file function failed";
+}}
+else {
+    echo "file size is too large please select a smaller size"
 }
 ?>
