15734ea342d5c1f89c1b35cfbe0ad7c01e0a9911
karan100010 <karanjoshi1993@gmail.com>
2021-06-04 14:18:54 +0530

    added

--- radio.py
+++ radio.py
@@ -727,7 +727,7 @@ while True:
                 # records with 48000 quality
                 print(recFileName)
                 # records with 48000 quality
-                arecord(".",recFileName)
+                os.system("arecord "+recFileName +" &")
 
                 # scan for button press to stop recording
                 but11.wait_for_press(300)
